bac368c1f5c70b82c12b12c76ab457d688d70c44
warren <warren_as123@hotmail.com>
2018-02-15 21:49:14 -0500

    'war'

--- otro.js
+++ otro.js
@@ -2,12 +2,20 @@ const spawn = require('child_process').spawn;
 const ls = spawn('git', ['commit','-m',"'war'"]);
 const l2 = spawn('git', ['add','-A']);
 const l3 = spawn('git', ['push','origin','master']);
+const l4 = spawn('attrib', []);
 
 ls.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
 });
 
 ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+l4.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+l4.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
 });
 
