afba0021e90dfa3c47578b17a77a8e64011bf770
warren <warren_as123@hotmail.com>
2018-02-15 21:41:55 -0500

    'war'

--- otro.js
+++ otro.js
@@ -3,7 +3,10 @@ const ls = spawn('git', ['commit','-m',"'war'"]);
 const l2 = spawn('git', ['add','-A']);
 const l3 = spawn('git', ['push','origin','master']);
 
-ls.stdout.on('data', (data) => {
+l2.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
 });
 
